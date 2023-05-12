c0cc758eeda9b7755f974d59a908670748674091
bhumika <sbhumika959@gmail.com>
2023-05-13 03:31:29 +0530

    second changes

--- src/main/java/com/security/Springboot/Security/config/MySecurityConfig.java
+++ src/main/java/com/security/Springboot/Security/config/MySecurityConfig.java
@@ -1,10 +1,12 @@
 package com.security.Springboot.Security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,11 +15,20 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests()
-                .antMatchers("/user/**").permitAll()
-                .antMatchers("/admin/**").denyAll()
+                .antMatchers("/user/home").permitAll()
+                .antMatchers("/user/dashboard").authenticated()
                 .and()
                 .httpBasic();
-
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("bhumi").password("12345").authorities("/admin")
+                .and()
+                .withUser("niha").password("4567").authorities("/user")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
 
     }
+
 }
