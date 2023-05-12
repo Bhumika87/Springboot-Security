2632605fba941fdfcdd1aaa483186f34f7d86486
bhumika <sbhumika959@gmail.com>
2023-05-13 04:14:40 +0530

    using user detail manager changes

--- src/main/java/com/security/Springboot/Security/config/MySecurityConfig.java
+++ src/main/java/com/security/Springboot/Security/config/MySecurityConfig.java
@@ -1,12 +1,19 @@
 package com.security.Springboot.Security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,15 +27,30 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("bhumi").password("12345").authorities("admin")
+//                .and()
+//                .withUser("niha").password("4567").authorities("user")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("bhumi").password("12345").authorities("/admin")
-                .and()
-                .withUser("niha").password("4567").authorities("/user")
-                .and()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        InMemoryUserDetailsManager userDetailsService =new InMemoryUserDetailsManager();
+        UserDetails user1= User.withUsername("arya").password("1234").authorities("admin").build();
+        UserDetails user2= User.withUsername("bharat").password("9876").authorities("user").build();
+        userDetailsService.createUser(user1);
+        userDetailsService.createUser(user2);
+        auth.userDetailsService(userDetailsService);
+
 
     }
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
